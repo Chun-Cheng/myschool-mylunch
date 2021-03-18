65d43aab28bf9fdfb12700ef4c399fad2b7c0016
Chun-Cheng <61136202+Chun-Cheng@users.noreply.github.com>
2021-03-18 11:24:18 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, redirect, url_for, jsonify  # flash render_template
+import pymongo
+from bson.objectid import ObjectId
+import re
+
+
+client = pymongo.MongoClient('mongodb+srv://dbUser:o5jzqcHzuKacB2Y1@lunchbox.1pvyu.mongodb.net/lunchbox?retryWrites=true&w=majority')
+database = client.mylunch
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = b'\xd9\xb9\x07\xad\x9d[\xe0e\xd2\x84\x9eU\xd8s\xe0\x15W\xcf\xf2n\x93\x01\xdc\xe4'  # 要換
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=180)
+
+
+@app.route('/')
+def index():
+    return 'hi~ This is myschool-mylunch(temporary name)'
+
+
+if __name__ == "__main__":
+    app.run()
