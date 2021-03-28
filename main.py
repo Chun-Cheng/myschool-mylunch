57dd27103c841c19c2eb7955997c9337a5c22a22
Chun-Cheng <61136202+Chun-Cheng@users.noreply.github.com>
2021-03-28 15:48:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -19,11 +19,11 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = b'\xd9\xb9\x07\xad\x9d[\xe0e\xd2\x84\x9eU\xd8s\xe0\x15W\xcf\xf2n\x93\x01\xdc\xe4'  # 要換
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=180)
 
-
+"""
 @app.route('/')
 def index():
     return 'hi~ This is myschool-mylunch(temporary name)'
-
+"""
 
 @app.route('/verify-token', methods=['GET','POST'])
 def verify_token():
