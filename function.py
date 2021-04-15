85b336def2635533ee2b07f940222286a7e13831
Chun-Cheng <61136202+Chun-Cheng@users.noreply.github.com>
2021-04-15 15:57:17 +0800

    Update function.py

--- function.py
+++ function.py
@@ -1,1 +1,43 @@
+import requests
+import json
+import pymongo
 
+
+
+def request_account_get(token, cols):
+    """
+    向帳號管理端請求使用者資料
+    input:
+      token
+      cols:list
+    output:
+      res_data
+      False
+    """
+    data = json.dumps({'token':str(token), 'cols':cols})
+    res = requests.post('https://myschool-account.herokuapp.com/account_data_request', data=data)
+    res_data = json.loads(res.text)
+    status_code = res.status_code
+    if status_code == 200:
+        return res_data
+    else:
+        return False
+
+    
+def request_token_check(token):
+    """
+    向帳號管理端確認用戶登入權杖是否有效
+    input:
+      token
+    output:
+      res_data['result']
+      status_code
+    """
+    data = {'token':}
+    res = requests.post('https://myschool-account.herokuapp.com/token_check_request', data=data)
+    res_data = json.loads(res.text)
+    status_code = res.status_code
+    if status_code == 200:
+        return res_data['result']
+    else:
+        return status_code
